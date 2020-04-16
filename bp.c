3f2f0d028220f7112d343a55481bba2e063e8052
TarantullJo <50945717+TarantullJo@users.noreply.github.com>
2020-04-16 18:08:51 +0300

    Add files via upload

---
+++ bp.c
@@ -0,0 +1,22 @@
+/* 046267 Computer Architecture - Spring 2020 - HW #1 */
+/* This file should hold your implementation of the predictor simulator */
+
+#include "bp_api.h"
+
+int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
+			bool isGlobalHist, bool isGlobalTable, int Shared){
+	return -1;
+}
+
+bool BP_predict(uint32_t pc, uint32_t *dst){
+	return false;
+}
+
+void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
+	return;
+}
+
+void BP_GetStats(SIM_stats *curStats){
+	return;
+}
+
